44c794b3e4d39001df87ee35b4794e4c3844be6b
h3llohihi <71027676+Dnezzhello@users.noreply.github.com>
2024-07-07 21:17:39 +0700

    update
    - set up first page

---
+++ Pc man Game MOO ICT 1/FirstPage.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Pc_man_Game_MOO_ICT_1
+{
+    partial class FirstPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FirstPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 526);
+            this.Name = "FirstPage";
+            this.Text = "FirstPage";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Pc man Game MOO ICT 1/FirstPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pc_man_Game_MOO_ICT_1
+{
+    public partial class FirstPage : Form
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
